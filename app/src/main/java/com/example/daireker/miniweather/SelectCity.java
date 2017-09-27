18522645bddc49726743fcb7e8eff89e7c4b9740
Daireker <243930237@qq.com>
2017-09-27 23:47:20 +0800

    second commit

---
+++ app/src/main/java/com/example/daireker/miniweather/SelectCity.java
@@ -0,0 +1,41 @@
+package com.example.daireker.miniweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by daireker on 2017/9/27.
+ */
+
+public class SelectCity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView mBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_city);
+
+        mBackBtn = (ImageView) findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.title_back:
+                Intent intent = new Intent();
+                intent.putExtra("cityCode","101160101");
+                setResult(RESULT_OK,intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
